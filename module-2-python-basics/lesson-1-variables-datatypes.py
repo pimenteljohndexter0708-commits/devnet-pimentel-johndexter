9bb06043d941b01cc2f4043f175f36826dd9a0f9
John Dexter A. Pimentel <pimentel.johndexter0708@gmail.com>
2026-09-25 18:37:53 +0800

    Done with the lesson 1 Varibles and datatypes

--- module-2-python-basics/lesson-1-variables-datatypes.py
+++ module-2-python-basics/lesson-1-variables-datatypes.py
@@ -1,7 +1,7 @@
 """
 Module 2 — Lesson 1: Variables & Data Types
-Student: [your name]
-Date: [date]
+Student: John Dexter Pimentel
+Date: 25/09/2026
 
 ============================================
 WHAT IS THIS TOPIC? (explain it like you're
@@ -9,16 +9,18 @@ teaching a friend who's never coded before)
 ============================================
 [write your own explanation here]
 
+This lesson is about Varaibles and Data Types. To explain this, I will use an analogy. The variable is like the shoe box and the shoe is the datatype.
+The variable can contain different types of data. While data types comes with different types. For example (int, string, float, boolean)
 
 ============================================
 KEY VOCABULARY
 ============================================
-- variable:
-- data type:
-- int:
-- float:
-- string:
-- boolean:
+- variable: Act as a container. It can store different data types.
+- data type: Data type is what you put on the variable. Comes with different types. For example (int, string, float, boolean).
+- int: int is a data type that can store whole numbers.
+- float: float is data type that can store floating number or numbers with decimal.
+- string: string is a data type that can store text, you can compose word using this data type.
+- boolean: boolean is just true or false.
 (add more as needed)
 
 
@@ -27,18 +29,25 @@ MY OWN EXAMPLE(S)
 ============================================
 Write at least one working example below that you
 came up with yourself — not copied from class.
-"""
-
-# --- your code example goes here ---
 
 
-"""
+# --- 
+# 
+  handsome_person = "dexie"
+  age = 23
+  weight = 99.12
+  is_handsome = True
+# 
+#  ---
+
 ============================================
 A MISTAKE I MADE (or one I want to avoid)
 ============================================
 [what's something confusing or easy to get wrong
 about this topic?]
 
+Sometimes when I'm assigning a value to an integer or float, I sometimes forgot that I don't need to put a quotation mark on it.
+Aside from that, there are times that I forgot the proper way in naming variables. Like you are not allowed to have a space in between the variable name.
 
 ============================================
 HOW THIS CONNECTS TO SOMETHING ELSE
