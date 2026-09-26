286fa966bb9502855ffe60058305ab4bc70d3b5f
Dex <pimentel.jonhdexter0708@gmail.com>
2026-09-26 08:52:41 +0800

    Done with the add device

--- Midterms/midterms.py
+++ Midterms/midterms.py
@@ -19,24 +19,22 @@ def display_menu():
     print("4. Find a device by name")
     print("5. Exit")
 
-    pass
 
-def add_device(name, ip):
+def add_device(device_list):
     # ask for name, IP, status — build the string, add to the list
 
     name = input("Enter your name: ")
-    ip = float("Enter your IP address")
+    ip = float(input("Enter your IP address: "))
 
     status = f"Name: {name}, IP: {ip}"
 
     devices.append(status)
 
-    return
-    pass
-
 def view_devices(device_list):
     # loop through and print every device — handle empty list
-    pass
+
+    for device in devices:
+        print(device)
 
 def count_active_inactive(device_list):
     # loop through, count Active vs Inactive, return both
@@ -44,7 +42,16 @@ def count_active_inactive(device_list):
 
 def find_device(device_list):
     # ask for a name, search the list, print result or "not found"
-    pass
+
+    name = input("Enter the name you want to find: ")
+
+    isthere = name in devices
+
+    if name in devices:
+        print("Found")
+    else:
+        print("Not found")
+
 
 # BONUS (optional)
 def remove_device(device_list):
@@ -59,17 +66,20 @@ def main():
         opt = int(input("Choose an option: "))
 
         if opt == 1:
-            add_device()
+            devices.append(add_device(devices))
+        
+            main()
+    
         elif opt == 2:
-            view_devices()
+            view_devices(devices)
+            main()
         elif opt == 3:
             count_active_inactive()
         elif opt == 4:
-            find_device()
+            find_device(devices)
         else:
             exit
         break
         # use if/elif to call the right function based on choice
         # set running = False when the user picks Exit
-
 main()
